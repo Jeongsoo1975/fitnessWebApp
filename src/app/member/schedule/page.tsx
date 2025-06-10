05df8bb94aa6ebef192583f07675f513d3e99780
Jeongsoo1975 <Jeongsoo1975@github.com>
2025-06-10 09:18:00 +0900

    feat: create member/schedule directory structure

---
+++ src/app/member/schedule/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+// Member Schedule Page - Will be implemented in next task
+export default function MemberSchedulePage() {
+  return (
+    <div>
+      <h1>Member Schedule Page</h1>
+      <p>Coming soon...</p>
+    </div>
+  )
+}
